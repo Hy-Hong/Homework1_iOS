5844daebdcb72317d9014f2898145f608f74123c
Hy Horng <hyhorng@hys-macbook-pro.local>
2020-09-09 17:51:49 +0700

    Initial Commit

---
+++ homeworkiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homeworkiOS
+//
+//  Created by Hy Horng on 9/9/20.
+//  Copyright © 2020 Hy Horng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
